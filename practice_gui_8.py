b0f7bd6754b21079ead87a57efb7d68a49cce8a0
Jesse Olson <jesstolson@gmail.com>
2017-10-02 00:45:45 -0400

    Create practice_gui_8.py

---
+++ practice_gui_8.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+
+class MonstersButtons:
+    def __init__(self, master): #second parameter for GUI, master means root or main window
+        frame = Frame(master)
+        frame.pack()
+
+        self.printButton = Button(frame, text="Print Message", command=self.printMessage)
+        self.printButton.pack(side=LEFT)
+
+        self.quitButton = Button(frame, text="Quit", command=frame.quit)
+        self.quitButton.pack(side=LEFT)
+
+    def printMessage(self):
+        print("ROAR, I AM BIGFOOT!!!")
+
+
+root = Tk()
+b = MonstersButtons(root) #calling MonstersButtons constructor
+root.mainloop()
