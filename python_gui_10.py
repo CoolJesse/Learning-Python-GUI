ba4d436e55d1934dcc20265c567e02477c0d8993
Jesse Olson <jesstolson@gmail.com>
2017-10-03 01:11:54 -0400

    Update python_gui_10.py

--- python_gui_10.py
+++ python_gui_10.py
@@ -12,29 +12,40 @@ root.geometry("500x300") #change default size of root
 Bigfoot_menu = Menu(root)
 root.config(menu=Bigfoot_menu)
 
-subMenu = Menu(Bigfoot_menu)
-Bigfoot_menu.add_cascade(label="File", menu=subMenu)
+fileMenu = Menu(Bigfoot_menu)
+Bigfoot_menu.add_cascade(label="File", menu=fileMenu)
 
-subMenu.add_command(label="Do you hear that?", command=BigfootSays)
-subMenu.add_separator()
+fileMenu.add_command(label="Look for Bigfoot", command=BigfootSays)
+fileMenu.add_separator()
 
-subMenu.add_command(label="Is that a Bigfoot I see?", command=BigfootSays)
-subMenu.add_separator()
+fileMenu.add_command(label="Try to catch Bigfoot", command=BigfootSays)
+fileMenu.add_separator()
 
-subMenu.add_command(label="Let's get out of here!", command=BigfootSays)
+fileMenu.add_command(label="Chase Bigfoot", command=BigfootSays)
+fileMenu.add_separator()
+
+fileMenu.add_command(label="Run away from Bigfoot!", command=BigfootSays)
+
 
 editMenu = Menu(Bigfoot_menu)
 Bigfoot_menu.add_cascade(label="Edit", menu=editMenu)
-editMenu.add_command(label="Redo", command=BigfootSays)
+
+editMenu.add_command(label="Rethink tactics", command=BigfootSays)
+editMenu.add_separator()
+
+editMenu.add_command(label="Set traps")
+editMenu.add_separator()
+
+editMenu.add_command(label="Get a big gun")
 
 #*******************************************************Toolbar*****************************************************#
 
 toolbar = Frame(root, bg="pink")
 
-insertHere = Button(toolbar, text="Insert Image", command = BigfootSays)
+insertHere = Button(toolbar, text="Tell your Bigfoot story", command = BigfootSays)
 insertHere.pack(side= LEFT, padx=2, pady=2)#pad gives extra space
 
-printHere = Button(toolbar, text="Print", command=BigfootSays)
+printHere = Button(toolbar, text="Contact a Bigfoot expert", command=BigfootSays)
 printHere.pack(side=LEFT, padx=2, pady=2)
 
 toolbar.pack(side=TOP, fill=X)
